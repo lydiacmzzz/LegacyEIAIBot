e58fdbfd44b4e01da0dad9085163eb74d734a3d6
CN <cnchen@rocketmail.com>
2024-01-19 12:04:56 +0000

    CN first commit.

---
+++ 2_Input_Repository_Files.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Input Repository")
+
+st.markdown("# Input Repository")
+st.sidebar.header("Input Repository")
+st.write(
+    """This function is currently in development"""
+)
---
+++ Welcome.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Welcome to EC Service Portal")
+
+st.markdown("# Welcome!")
+st.sidebar.success("Select a function above.")
+
+st.write(
+    """
+    Welcome to the EC Service Portal! Ask about ECI Programmes by clicking on the EC Services Bot...
+    """
+)
