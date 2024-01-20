bc6dcdab00e0635101695aee014c5ea3cfd5bfa9
Zen <lohzg.ai@gmail.com>
2024-01-20 12:36:55 +0800

    update config.toml

--- pages/1_EI Service Chatbot.py
+++ pages/1_EI Service Chatbot.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.write("Chatbot Page")
+st.set_page_config(
+    page_title = "EI Service Chatbot",
+    page_icon = "💻",
+)
+st.title("EI Service Chatbot")
+st.caption("EI Service Chatbot powered by AWS & Streamlit")
+
+# Initialize chat history in session state
+if "chat_history" not in st.session_state:
+    st.session_state['chat_history'] = []
+        
+st.write(st.session_state)
