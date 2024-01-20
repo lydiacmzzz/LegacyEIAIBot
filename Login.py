c4113037fb739815e7103ece108488e2f4679680
Zen <lohzg.ai@gmail.com>
2024-01-20 18:43:39 +0800

    login update

--- Login.py
+++ Login.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import os
 
 st.write("Login Page")
+
+
+def save_uploadedfile(uploadedfile):
+    with open(os.path.join("uploaded",uploadedfile.name),"wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success(f"Saved File: {uploadedfile.name} to uploaded")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    save_uploadedfile(uploaded_file)
