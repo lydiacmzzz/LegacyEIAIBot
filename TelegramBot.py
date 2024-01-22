ef3cdadcb3b7f187df4a1d760a55b2463f7c700d
Joey <joey_lee@msf.gov.sg>
2024-01-22 05:58:55 +0000

    Telegram Bot with whitelisting feature.

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,52 +1,40 @@
-# from dotenv import load_dotenv
-# load_dotenv()
-import openai
 import sys
 import os
 import requests
 import json
 import threading
 import kendra_chat_bedrock_claudev2 as bedrock_claudev2
+import csv
 
+csv_file_path = 'whitelisted_users.csv'
+whitelisted_users = []
+try:
+    with open(csv_file_path, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        
+        # Iterate through each row in the CSV file
+        for row in reader:
+            # Assuming there is only one column in the CSV
+            whitelisted_users.append(row[0])
+except FileNotFoundError:
+    print(f"Error: File '{csv_file_path}' not found.")
+except Exception as e:
+    print(f"Error: {e}")
+
+print("Whitelisted Users:", whitelisted_users)
 
 # Telegram secret access bot token
 BOT_TOKEN = os.environ['TELEGRAMBOT']
-
-# openai.api_type = "azure"
-# openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT") 
-# openai.api_version = "2023-03-15-preview"
-# openai.api_key = os.getenv("AZURE_OPENAI_KEY")
-
-# print("Azure OpenAi Endpoint :" + openai.api_base)
-# print("Azure OpenAi Key : " + openai.api_key)
 
 chat_history = []
 input = ""
 
 def chatbot(input_text):
-    #index = GPTSimpleVectorIndex.load_from_disk('index.json')
     
     llm_chain = bedrock_claudev2.build_chain()
     result = bedrock_claudev2.run_chain(llm_chain, input_text, chat_history)
     answer = result['answer']
     chat_history.append((input, answer))
-    
-    # response = openai.ChatCompletion.create(
-    #   engine="gpt35-turbo-poc001",
-    #   messages = [
-    #       {"role":"system","content":"Assistant is a large language model trained by OpenAI."},
-    #       {"role":"user","content":input_text}],
-    #   temperature=0.7,
-    #   max_tokens=800,
-    #   top_p=0.95,
-    #   frequency_penalty=0,
-    #   presence_penalty=0,
-    #   stop=None)
-  
-#     print(response)
-#     print(response['choices'][0]['message']['content'])
-    
-#     reply = response['choices'][0]['message']['content']
     
     return answer
 
@@ -141,33 +129,50 @@ def Chatbot():
                     
                     print(msg_id,chat_id,user_name,question)
                     
-                    # Checking that user mentionned chatbot's username in message
-                    #if '@SAO_GenAi_bot' in result['message']['text']:
-                    #prompt = result['message']['text'].replace("@SAO_GenAi_bot", "")
-                
-                    # Calling OpenAI API using the bot's personality
-                    #bot_response = openAI(f"{BOT_PERSONALITY}{prompt}")
-                    if 'reply_to_message' not in result['message']:
-                        prompt = result['message']['text']
-                        bot_response = chatbot(prompt)
-                        # Sending back response to telegram group
-                        print(telegram_bot_sendtext(bot_response, chat_id, msg_id))
-                        
-                        #logging
-                        log = "Requestor: " + user_name + "\nQuestion: " + question + "\nAnswer: " + bot_response
-                        print(telegram_bot_sendlog(log)) 
-                        
-                    # Verifying that the user is responding to the ChatGPT bot
-                    if 'reply_to_message' in result['message']:
-                        if result['message']['reply_to_message']['from']['is_bot']:
+                    if chat_id in whitelisted_users:                   
+
+                        if 'reply_to_message' not in result['message']:
                             prompt = result['message']['text']
-                            #bot_response = openAI(f"{BOT_PERSONALITY}{prompt}")
                             bot_response = chatbot(prompt)
+                            # Sending back response to telegram group
                             print(telegram_bot_sendtext(bot_response, chat_id, msg_id))
 
                             #logging
                             log = "Requestor: " + user_name + "\nQuestion: " + question + "\nAnswer: " + bot_response
-                            print(telegram_bot_sendlog(log))
+                            print(telegram_bot_sendlog(log)) 
+
+                        # Verifying that the user is responding to the ChatGPT bot
+                        if 'reply_to_message' in result['message']:
+                            if result['message']['reply_to_message']['from']['is_bot']:
+                                prompt = result['message']['text']
+                                #bot_response = openAI(f"{BOT_PERSONALITY}{prompt}")
+                                bot_response = chatbot(prompt)
+                                print(telegram_bot_sendtext(bot_response, chat_id, msg_id))
+
+                                #logging
+                                log = "Requestor: " + user_name + "\nQuestion: " + question + "\nAnswer: " + bot_response
+                                print(telegram_bot_sendlog(log))
+                                
+                    else:
+                        print("Sorry, you are not whitelisted")
+                        if 'reply_to_message' not in result['message']:
+                            bot_response = "Sorry, you are not whitelisted"
+                            # Sending back response to telegram group
+                            print(telegram_bot_sendtext(bot_response, chat_id, msg_id))
+
+                            #logging
+                            log = "Requestor: " + user_name + "\nQuestion: " + question + "\nAnswer: " + bot_response
+                            print(telegram_bot_sendlog(log)) 
+
+                        # Verifying that the user is responding to the ChatGPT bot
+                        if 'reply_to_message' in result['message']:
+                            if result['message']['reply_to_message']['from']['is_bot']:
+                                bot_response = "Sorry, you are not whitelisted"
+                                print(telegram_bot_sendtext(bot_response, chat_id, msg_id))
+
+                                #logging
+                                log = "Requestor: " + user_name + "\nQuestion: " + question + "\nAnswer: " + bot_response
+                                print(telegram_bot_sendlog(log))
                             
         except Exception as e: 
             print(e)
